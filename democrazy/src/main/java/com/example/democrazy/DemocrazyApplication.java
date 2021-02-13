5e4cbb991a18e4353fcb4058d419322ffc357ea4
relivnd <77431277+relivnd@users.noreply.github.com>
2021-02-13 21:43:23 +0100

    Add files via upload

---
+++ democrazy/src/main/java/com/example/democrazy/DemocrazyApplication.java
@@ -0,0 +1,13 @@
+package com.example.democrazy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocrazyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocrazyApplication.class, args);
+	}
+
+}
---
+++ democrazy/src/test/java/com/example/democrazy/DemocrazyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.democrazy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemocrazyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
