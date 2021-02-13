94664d57415f28c5562a3bcfb38066d77eb16c94
Eddy-unine <edoaurd.tang@unine.ch>
2021-02-13 22:09:14 +0100

    add question component add bootstrap

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { QuestionComponent } from './question/question.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule
