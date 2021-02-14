60a6a4bcc25f10b3c0d53364c6b5670e8e473eb9
relivnd <hej+github@relivnd.dev>
2021-02-14 20:58:19 +0100

    Added getAll Method to REST Controller

--- democrazy/src/main/java/com/example/democrazy/rest/QuestionsRestController.java
+++ democrazy/src/main/java/com/example/democrazy/rest/QuestionsRestController.java
@@ -1,4 +1,27 @@
 package com.example.democrazy.rest;
 
+import com.example.democrazy.model.Question;
+import com.example.democrazy.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1")
 public class QuestionsRestController {
+
+    private final QuestionService questionService;
+
+    @Autowired
+    public QuestionsRestController(QuestionService questionService){
+        this.questionService = questionService;
+    }
+
+    @GetMapping
+    public List<Question> getAll(){
+        return questionService.getAllQuestions();
+    }
 }
--- democrazy/src/main/java/com/example/democrazy/service/QuestionService.java
+++ democrazy/src/main/java/com/example/democrazy/service/QuestionService.java
@@ -3,9 +3,11 @@ package com.example.democrazy.service;
 import com.example.democrazy.dao.QuestionRepository;
 import com.example.democrazy.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class QuestionService {
 
     private final QuestionRepository repository;
