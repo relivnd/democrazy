8bd0e07211afe7398da2cf0788db83ad48a3d2ae
relivnd <hej+github@relivnd.dev>
2021-02-14 20:20:40 +0100

    Added Attributes to Answers with Lombok Annotations

--- democrazy/src/main/java/com/example/democrazy/model/Answers.java
+++ democrazy/src/main/java/com/example/democrazy/model/Answers.java
@@ -1,4 +1,30 @@
 package com.example.democrazy.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class Answers {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private boolean answer;
+
+    @OneToMany
+    @JoinColumn(name = "ANSWER_ID")
+    private final List<Democrates> democrates = new ArrayList<>();
+
+    @OneToMany
+    @JoinColumn(name = "ANSWER_ID")
+    private final List<Question> questions = new ArrayList<>();
 }
