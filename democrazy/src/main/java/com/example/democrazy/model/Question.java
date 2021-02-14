daf483988fce669c5cf4e9a0739213bab0d8ec12
relivnd <hej+github@relivnd.dev>
2021-02-14 20:11:31 +0100

    Added Attributes to Question.java with Lombok Annotations

--- democrazy/src/main/java/com/example/democrazy/model/Question.java
+++ democrazy/src/main/java/com/example/democrazy/model/Question.java
@@ -1,4 +1,21 @@
 package com.example.democrazy.model;
 
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter @NoArgsConstructor @ToString @EqualsAndHashCode
 public class Question {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String questionString;
+
+    private String answerOne;
+
+    private String answerTwo;
 }
