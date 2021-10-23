c240a7beeda9ce9541bcc23625319ded5f4471fc
GNUSMAS <edouard.tang@unine.ch>
2021-10-23 16:04:10 +0200

    add Endpoint

---
+++ backend/Democrazy/Democrazy/Controllers/PollController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication.Data;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    [Route("api/polls")]
+    [ApiController]
+    public class PollController : Controller
+    {
+        private readonly PollRepo _pollRepo = new PollRepo();
+
+        [HttpGet("{id}")]
+        public ActionResult<Poll> GetPollById(int id)
+        {
+            var pollItem = _pollRepo.GetPollById(id);
+            return Ok(pollItem);
+        }
+    }
+}
